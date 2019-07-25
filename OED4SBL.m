38be9e2e0a3de6d199f50fb34b56a144c12bfac3
davidh <davidh@iim.csic.es>
2019-07-25 17:21:48 +0100

    Update OED4SBL.m

--- OED4SBL.m
+++ OED4SBL.m
@@ -1,26 +1,24 @@
-function [ output_args ] = OED4SBL(model)
-
-
+function [ output_args ] = OED4SBL(model,inputs)
 
     % Add new experiment that is to be designed
-    inputs.exps.n_exp = inputs.exps.n_exp + 1;                      % Number of experiments
-    iexp = inputs.exps.n_exp;                                       % Index of the experiment
-    inputs.exps.exp_type{iexp}='od';                                % Specify the type of experiment: 'od' optimally designed
-    inputs.exps.n_obs{iexp}=2;                                      % Number of observables in the experiment
-    inputs.exps.obs_names{iexp}=char('RFP','GFP');               % Name of the observables in the experiment
-    inputs.exps.obs{iexp}=char('RFP = L_RFP','GFP = T_GFP');          % Observation function
+    inputs.exps.n_exp = inputs.exps.n_exp + 1;               
+    iexp = inputs.exps.n_exp;                                
+    inputs.exps.exp_type{iexp}='od';                         
+    inputs.exps.n_obs{iexp}=2;                               
+    inputs.exps.obs_names{iexp}=inputs.exps.obs_names{iexp-1}
+    inputs.exps.obs{iexp}=inputs.exps.obs{iexp-1};       
     
-    % Fixed parts of the experiment
-    inputs.exps.exp_y0{iexp}=oid_y0;                                % Initial conditions
-    inputs.exps.t_f{iexp}=duration;                                 % Duration of the experiment (minutes)
-    inputs.exps.n_s{iexp}=duration/5+1;                             % Number of sampling times - sample every 5 min
+
+    %% Fixed parts of the experiment
+    inputs.exps.exp_y0{iexp}=oid_y0;                                
+    inputs.exps.t_f{iexp}=duration;                                 
+    inputs.exps.n_s{iexp}=duration/5+1;                             
     
-    % OED of the input
+    %% OED of the input
     inputs.exps.u_type{iexp}='od';
-    inputs.exps.u_interp{iexp}='stepf';                             % Stimuli definition for experiment: 'stepf' steps of constant duration
-    inputs.exps.n_steps{iexp}=round(duration/stepDuration);         % Number of steps in the input
-    inputs.exps.t_con{iexp}=0:stepDuration:(duration);            % Switching times
-    
+    inputs.exps.u_interp{iexp}='stepf';                             
+    inputs.exps.n_steps{iexp}=round(duration/stepDuration);         
+    inputs.exps.t_con{iexp}=0:stepDuration:(duration);                
     inputs.exps.u_min{iexp}= [0*ones(1,inputs.exps.n_steps{iexp}); 0*ones(1,inputs.exps.n_steps{iexp})]+1e-7;
     inputs.exps.u_max{iexp}=[1*ones(1,inputs.exps.n_steps{iexp}); 100*ones(1,inputs.exps.n_steps{iexp})];
     
@@ -52,6 +50,8 @@ function [ output_args ] = OED4SBL(model)
     inputs.pathd.short_name     = short_name;
     inputs.pathd.runident       = strcat('oed-',int2str(i));
         
+    [inputs,privstruct]=AMIGO_Prep(inputs);
+    
     results = AMIGO_OED(inputs);
     oed_results{i} = results;
     
