8eed5b2d14ab13009e8a4e250703ba3965d5a1b5
zoltuz <zoltuz@gmail.com>
2019-07-25 12:15:29 +0100

    moving data files and updating data reader for SBL

--- datareader_for_SBL.m
+++ datareader_for_SBL.m
@@ -1,14 +1,27 @@
 %% ZAT ICL March 2019
-function input_data = datareader_for_SBL(file)
+function input_data = datareader_for_SBL(dir_name,file_name,exp_idx)
 
-T = readtable(file);
+
+T = readtable([dir_name '/' file_name]);
 % capitalize variable names 
 T.Properties.VariableNames = upper(T.Properties.VariableNames);
-input_idx_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'TR_\w+'));
-output_idx_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'READOUT_\w+'));
-time_idx_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'TIME'));
-% TODO impement SMV vs MMV mode
-exp_num = max(table2array(T(:,1)));
+exp_id_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'^EXPERIMENT'));
+input_idx_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'^TR_\w+'));
+output_idx_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'^READOUT_\w+'));
+time_idx_vec = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'^TIME'));
+std_idx_vec  = ~cellfun(@isempty,regexp(T.Properties.VariableNames,'^STD_\w+')); 
+
+
+
+%% get names
+token_input = regexp(T.Properties.VariableNames,'^TR_(\w+)','once','tokens');
+input_data.input_names = [token_input{input_idx_vec}];
+
+token_state = regexp(T.Properties.VariableNames,'^READOUT_(\w+)','once','tokens');
+input_data.state_names = [token_state{output_idx_vec}];
+
+
+%% finding the columns
 
 time_idx = find(time_idx_vec==1);
 assert(numel(time_idx) == 1)
@@ -17,29 +30,42 @@ input_idx = find(input_idx_vec ==1);
 
 output_idx = find(output_idx_vec ==1);
 assert(numel(output_idx) >=1)
+input_data.state_num = numel(output_idx);
 
-% 
-% % TODO: detect NaN
-% idx = find(any(isnan(M)'));
-% if ~isempty(idx)
-%     read_until = idx(1)-1;
-%     warning('NaN detected during import, after NaN data is ignored!!!')
-% else
-%     read_until = size(M,1);
-% end
+if sum(std_idx_vec>0)
+std_idx = find(std_idx_vec ==1);
+assert(numel(output_idx) == numel(std_idx))
+end
+
+exp_idx = find(exp_id_vec ==1);
+assert(numel(exp_idx) == 1)
+
+%% determining exp num
 
 
+exp_ids = unique(sort(table2array(T(:,exp_idx))));
+
+for k=1:size(exp_ids,1)
+    exp_range = find(table2array(T(:,exp_idx)) == exp_ids(k));
 % time vector
-input_data.tspan = table2array(T(:,time_idx));
+input_data.tspan{k} = table2array(T(exp_range,time_idx));
 % inputs
 if ~isempty(input_idx)
-    input_data.inputs = table2array(T(:,input_idx));
+    % trimming non-zero values created during import
+    tmp =table2array(T(exp_range,input_idx));
+    tmp(tmp<=1e-6) = 0;
+    input_data.inputs{k} = tmp;
 else
-    input_data.inputs = [];
+    input_data.inputs{k} = [];
 end
 % states
-input_data.states = table2array(T(:,output_idx));
+input_data.states{k} = table2array(T(exp_range,output_idx));
+% measurement noise
+if ~isempty(std_idx)
+    input_data.input_std{k} = table2array(T(exp_range,std_idx));
+end
 
+end
 
 disp('import was successfull!')
 
