580ab4d5b060cb3e760e63da15ea7a864bbdd4c8
davidh <davidh@iim.csic.es>
2019-07-25 13:58:19 +0100

    Update generate_model_char.m

--- SBL/generate_model_char.m
+++ SBL/generate_model_char.m
@@ -21,7 +21,6 @@ for k=1:stat_num
     % converting dictionary function to model string
     dict_str  = cellfun(@(x) replace(func2str(x),{'@(x,p)ones(size(x,1),1)','@(x,u)ones(size(x,1),1)','@(x,u)','@(x,p)','@(x)'},{'1','1','','',''}),Phi{k},'UniformOutput',false)';
     dict_str  = cellfun(@(x) regexprep(x,{'x\(:,(\d)\)','u\(:,(\d)\)'},{'x$1','u$1'}),dict_str,'UniformOutput',false);
-    %strrep()
     
     if isempty(model.state_names)
         state_names{k} = sprintf('x%d',k);
