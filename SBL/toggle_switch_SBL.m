87cf1145e055290c69286d1bfee6c51a6a3d0033
zoltuz <zoltuz@gmail.com>
2019-07-26 15:14:21 +0100

    development for the loop mode

---
+++ SBL/toggle_switch_SBL.m
@@ -0,0 +1,119 @@
+function [Phi,fit_res_diff,model,input_data] = toggle_switch_SBL(input_data,config)
+
+% generating the dictionary functions
+Phi = build_toggle_switch_dict();
+
+state_num = input_data.state_num;
+exp_num = size(input_data.tspan,2);
+
+model.experiment_num = exp_num;
+model.state_names = input_data.state_names;
+model.input_names = input_data.input_names;
+
+% check for sparsity cases
+if isfield(config,'sparsity_vec')
+    sparsity_vec = config.sparsity_vec;
+else
+    sparsity_vec = 0.2;
+end
+
+%% for each sparsity
+for sparsity_case=1:size(sparsity_vec,2)
+    %% for each dataset
+    for exp_idx=1:exp_num
+        if config.display_plots
+            figure('name','datafit')
+        end
+        % step differeniate the signal
+        dydt =[];
+        for state = 1:state_num
+            y_tmp = input_data.states{exp_idx}(:,state);
+            f = fit(input_data.tspan{exp_idx},y_tmp,'smoothingspline','SmoothingParam',0.00001);
+            dydt(:,state) = differentiate(f,input_data.tspan{exp_idx});
+            if config.display_plots
+                subplot(state_num,exp_num,sub2ind([state_num exp_num],state,exp_idxx))
+                plot(f,input_data.tspan{exp_idx},input_data.states{exp_idx}(:,state))
+            end
+        end
+        
+        model.dydt{exp_idx} = dydt;
+        model.variance{exp_idx} = sparsity_vec(sparsity_case);
+        model.tspan{exp_idx} = input_data.tspan{exp_idx};
+        
+        model.input{exp_idx} = input_data.inputs{exp_idx};
+        
+        %% evaexp_idxuate dictionary
+        
+        x = [input_data.states{exp_idx}  input_data.inputs{exp_idx}];
+        % extra (not estimated) parameters
+        p = [];
+        % the dicionaries is evaluated for all states and datasets
+        Phi_val = cell(state_num,exp_num);
+        
+        
+        for state = 1:state_num
+            Phi_val{state,exp_idx} = cell2mat(cellfun(@(f) f(x,p),Phi{state},'UniformOutput',false));
+        end
+        
+        %% buiexp_idxd a exp_idxinear regression struct, i.e. y = A*x for each states and data sets
+        for state=1:state_num
+            sbl_diff(state).name = sprintf('diff_%s',model.state_names{state});
+            sbl_diff(state).A{exp_idx} = Phi_val{state,exp_idx};
+            sbl_diff(state).y{exp_idx} = model.dydt{exp_idx}(:,state);
+            sbl_diff(state).std{exp_idx} = model.variance{exp_idx};
+        end
+        
+    end % for each dataset
+    %% generate nonneg constraints
+    for state=1:state_num
+        constraint_idx= [];
+        for z=2:size(Phi{state},2)
+            if isempty(strfind(func2str(Phi{state}{z}),sprintf('x(:,%d)',state)))
+                constraint_idx = [constraint_idx; z];
+            end
+        end
+        sbl_config(state).nonneg{1} = constraint_idx;
+        % estimate only the selected states
+        sbl_config(state).selected_states = 1:size(model.dydt,2);
+        
+    end
+    %% run sbl
+    tic;
+    for state=1:state_num
+        sbl_config(state).max_iter = 10;
+        sbl_config(state).mode = 'SMV';
+        fit_res_diff(state,sparsity_case) = vec_sbl(sbl_diff(state),sbl_config(state),model);
+    end
+    toc;
+    %% reporting
+    for state=1:state_num
+        % use manual threshold
+        zero_th = 1e-4;
+        model_num = 1;
+        % seexp_idxect non zero dictionaries
+        fit_res_diff(state,sparsity_case) = calc_zero_th(fit_res_diff(state,sparsity_case),zero_th,config.display_plots,model_num);
+        % report signal fit
+        signal_fit_error_diff(state,sparsity_case) = fit_report(fit_res_diff(state,sparsity_case),config.display_plots);
+    end
+    %% simuexp_idxate the reconstructed ODE
+    
+    % zero out the constant term
+    for state=1:state_num
+        fit_res_diff(state).w_est{1}(1) = 0;
+        idx = find(fit_res_diff(state,sparsity_case).non_zero_dict{1} == 1);
+        assert(numel(idx)<2)
+        fit_res_diff(state,sparsity_case).non_zero_dict{1}(idx) = [];
+    end
+    
+    valid_model = simulateSBLresults(Phi,fit_res_diff(:,sparsity_case),model,config.display_plots);
+    for state=1:state_num
+        fit_res_diff(state,sparsity_case).valid_model = valid_model;
+    end
+    
+end % for each sparsity case
+
+
+%% save the resuexp_idxts to a mat fiexp_idxe
+if config.save_results_to_mat
+    save([dir_name '/toggle_switch_sbl_output'],'Phi','fit_res_diff','model','input_data')
+end
--- loop_main.m
+++ loop_main.m
@@ -10,6 +10,13 @@ close all
 loop_iter = 2;
 fid = 1; % display on the screen
 
+%% SBL config
+sbl_config.display_plots  = 0;
+sbl_config.save_results_to_mat = 0;
+% generating different model structures
+sbl_config.sparsity_vec = [0.02 0.2 2];
+
+%% first dataset
 data_dir_name = 'Data';
 % first data set
 data_file_name = 'experimental_data_7exps_noise000.csv';
@@ -17,47 +24,57 @@ data_file_name = 'experimental_data_7exps_noise000.csv';
 %% start computation
 for loop = 1:loop_iter
     
-%% Step 1: generate data for SBL
-logger(fid,sprintf('loop iter: %d, generating SBL data',loop))
-
-% select the experiments to be considered
-exp_idx = [1];
-% load data from a file
-input_data = datareader_for_SBL(data_dir_name,data_file_name,exp_idx,fid);
-
-%% Step 2: Run SBL 
-logger(fid,sprintf('loop iter: %d, running SBL',loop))
-
-%% Step 3: Build an Amigo model
-logger(fid,sprintf('loop iter: %d, building an Amigo model',loop))
-
-% RES={};
-% clear mex;
-% 
-% for sparsity_case=1:size(sparsity_vec,2)
-%  
-%         SBLModel = SBLModel2AMIGOModel(fit_res_diff(:,sparsity_case),Phi,model,['SBL' num2str(sparsity_case)]);
-%         
-%         [inputs privstruct]=gen_AMIGOSetupFromSBL(SBLModel,'experimental_data_exp1to1_noise000.csv','SBLModel');
-%         
-% %         [inputs,privstruct,res_ssm]=fit_SBLModel(inputs,privstruct);
-% %         RES={inputs,privstruct,res_ssm};
-%             
-% end
-
-%% Step 4: Run parameter estimation in Amigo
-logger(fid,sprintf('loop iter: %d, running parameter est in Amigo',loop))
-
-
-%% Step 5: Run OED
-logger(fid,sprintf('loop iter: %d, running the OED',loop))
-
-
-
-%% Step 6: generate new set of data     
-logger(fid,sprintf('loop iter: %d, generating new set of data',loop))
-
-data_file_name = '';
-
-logger(fid,sprintf('loop iter: %d is DONE',loop))    
+    %% Step 1: generate data for SBL
+    logger(fid,sprintf('loop iter: %d, generating SBL data',loop))
+    
+    % select the experiments to be considered
+    exp_idx = [1];
+    % load data from a file
+    input_data = datareader_for_SBL(data_dir_name,data_file_name,exp_idx,fid);
+    
+    %% Step 2: Run SBL
+    logger(fid,sprintf('loop iter: %d, running SBL',loop))
+    
+    [Phi,fit_res_diff,model]  = toggle_switch_SBL(input_data,sbl_config);
+    
+    %% Step 3: Build an Amigo model
+    logger(fid,sprintf('loop iter: %d, building an Amigo model',loop))
+    
+    RES={};
+    clear mex;
+    
+    imported_to_amigo = 0;
+    for sparsity_case=1:size(sbl_config.sparsity_vec,2)
+        
+        if all([fit_res_diff(:,sparsity_case).valid_model] == true)
+            SBLModel = SBLModel2AMIGOModel(fit_res_diff(:,sparsity_case),Phi,model,['SBL' num2str(sparsity_case)]);
+            
+            [inputs privstruct]=gen_AMIGOSetupFromSBL(SBLModel,'experimental_data_exp1to1_noise000.csv','SBLModel');
+            imported_to_amigo = imported_to_amigo +1;
+        end
+        
+        %         [inputs,privstruct,res_ssm]=fit_SBLModel(inputs,privstruct);
+        %         RES={inputs,privstruct,res_ssm};
+        
+    end
+    if imported_to_amigo == 0
+        logger(fid,sprintf('loop iter: %d, there is no valid model to work with, LOOP STOPS',loop))
+        break
+    end
+    
+    %% Step 4: Run parameter estimation in Amigo
+    logger(fid,sprintf('loop iter: %d, running parameter est in Amigo',loop))
+    
+    
+    %% Step 5: Run OED
+    logger(fid,sprintf('loop iter: %d, running the OED',loop))
+    
+    
+    
+    %% Step 6: generate new set of data
+    logger(fid,sprintf('loop iter: %d, generating new set of data',loop))
+    
+    data_file_name = '';
+    
+    logger(fid,sprintf('loop iter: %d is DONE',loop))
 end % end of loop for
