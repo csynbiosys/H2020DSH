29dc5096deb6b1f40e511468db95ce5fb8cdb1e1
davidh <davidh@iim.csic.es>
2019-09-25 11:11:12 +0100

    A

--- Examples/ToggleSwitch/toggle_4_2.m
+++ Examples/ToggleSwitch/toggle_4_2.m
@@ -12,3 +12,6 @@ SBL_config_defaults;
 sbl_config.data_dir_name = pwd;
 sbl_config.data_file_name = 'toggleSwitch_1.csv';
 sbl_config.exp_idx=1:3;
+sbl_config.dict_generator = str2func('build_toggle_switch_dict');
+sbl_config.estimate_structure_only=0; 
+sbl_config.sparsity_vec = [0.05 0.1 0.15 0.2 ];
--- Examples/ToggleSwitch/toggle_4_4_4.m
+++ Examples/ToggleSwitch/toggle_4_4_4.m
@@ -10,12 +10,14 @@ SBL_config_defaults;
 sbl_config.data_dir_name = pwd;
 sbl_config.data_file_name = 'toggleSwitch_1.csv';
 sbl_config.exp_idx=1:3;
+sbl_config.dict_generator = str2func('build_toggle_switch_dict');
+sbl_config.estimate_structure_only=0; 
 
 %% Generate multiple models by enforcing different sparsity coeficients.
 % 
 %
 
-sbl_config.sparsity_vec = [0.15 0.2 0.25 0.3];
+sbl_config.sparsity_vec = [0.05 0.1 0.15 0.2 ];
 MODELS=SBL_gen_model_family(sbl_config);
 
 %% Compute AIC and RANK parameters 
